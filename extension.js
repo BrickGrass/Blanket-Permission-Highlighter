2c920ae8137ae0da289c12caf67c996b245465ba
George <George>
2020-09-20 01:51:14 +0100

    Inject link to fps list author page to the ao3 profile page

--- extension.js
+++ extension.js
@@ -5,23 +5,29 @@
 // @description  Highlights authors on ao3 who have a blanket permission statement
 // @author       BrickGrass
 // @include      https://archiveofourown.org/*
+// @grant        GM_xmlhttpRequest
 // @require      http://code.jquery.com/jquery-3.5.1.min.js
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 // ==/UserScript==
 
-const user_regex = new RegExp('https:\/\/archiveofourown\.org\/users\/([^/]+)\/pseuds\/([^/]+)');
+const user_regex = /^https:\/\/archiveofourown\.org\/users\/([^/]+)\/pseuds\/([^/]+)$/;
 var users_exist = [];
 
-function bp_exists(username, callback) {
+function bp_exists(username, context, callback) {
     $.ajax(
         `https://brickgrass.uk/bp_api/author_exists/${username}`,
-        {
-            method: "POST"
-        }
+        {context: context}
     ).done(callback);
 }
 
-$(document).ready(function() {
+function bp_fetch(username, callback) {
+    $.ajax(
+        `https://brickgrass.uk/bp_api/author_data/${username}`,
+        {context: context}
+    ).done(callback);
+}
+
+$( document ).ready(function() {
     console.log("Highlighting authors with bp...");
     $("a").each(function() {
         let m = this.href.match(user_regex);
@@ -35,13 +41,26 @@ $(document).ready(function() {
             return;
         }
 
-        bp_exists(m[1], function(data) {
+        bp_exists(m[1], this, function(data) {
             if (data.exists) {
-                this.css({color: "#0f782d"});
+                $(this).css({color: "#0f782d"});
                 users_exist.push(m[1]);
             }
         })
     });
 
-    // Check url to see if it's a profile, handle accordingly
+    let m = window.location.href.match(user_regex);
+    if (m === null) {
+        return;
+    }
+
+    console.log("On a profile, fetching bp info...")
+
+    bp_fetch(m[1], function(data) {
+        console.log(data);
+
+        $("#dashboard ul").last().append(
+            `<li><a href="${data.author}">FPS List Entry</a></li>`
+        )
+    });
 });
