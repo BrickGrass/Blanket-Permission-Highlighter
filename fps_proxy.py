5631418b60a13af0bce62d515796fb7ae0a699f8
George <49255229+BrickGrass@users.noreply.github.com>
2021-12-14 23:35:05 +0000

    Use redis for caching. The redis server is configured to have a max memory of ~250MB and use allkeys-lfu for eviction

--- fps_proxy.py
+++ fps_proxy.py
@@ -1,9 +1,10 @@
+from datetime import timedelta
 import json
 
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 from bs4 import BeautifulSoup
-from memoization import cached, CachingAlgorithmFlag
+from redis import Redis
 from flask import Flask, jsonify
 
 import fps_get
@@ -20,26 +21,32 @@ sentry_sdk.init(
 
 sess = fps_get.Session()
 app = Flask(__name__)
-_24_hours = 24 * 60 * 60
+_24_hours = timedelta(days=1)
+
+r = Redis()
 
 
-@cached(max_size=750, algorithm=CachingAlgorithmFlag.LFU, ttl=_24_hours)
 def fetch_author(username):
+    if value := r.get(username):
+        return None if value == b"n" else {"author": value.decode("utf-8")}
+
     try:
         data = sess.get_author(username)
     except fps_get.NonceExpiredError:
         sess.form.nonce = sess.get_nonce()
         data = sess.get_author(username)
 
-    if data["recordsFiltered"] == 0:
-        return None
+    if data["recordsFiltered"] != 0:
+        for _, author_page, _, _, _ in data["data"]:
+            author_page = BeautifulSoup(author_page)
 
-    for _, author_page, _, _, statement in data["data"]:
-        author_page = BeautifulSoup(author_page)
-        statement = BeautifulSoup(statement)
+            if author_page.string.lower() == username.lower():
+                author_data = author_page.a["href"]
+                r.setex(username, _24_hours, author_data)
+                return {"author": author_data}
 
-        if author_page.string.lower() == username.lower():
-            return {"author": author_page.a["href"], "statement": statement.a["href"]}
+    r.setex(username, _24_hours, "n")
+    return None
 
 
 @app.route("/bp_api/author_exists/<username>")
@@ -57,14 +64,3 @@ def author_data(username):
 
     author["message"] = "found"
     return jsonify(author), 200
-
-
-@app.route("/bp_api/cache_health")
-def cache_health():
-    cache_info = fetch_author.cache_info()
-    cache_health = {
-        "hits": cache_info.hits,
-        "misses": cache_info.misses,
-        "current_size": cache_info.current_size,
-    }
-    return jsonify(cache_health), 200
