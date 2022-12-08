15c2a5031a2de5d89aa4eb6dd85ad92c1bfe030a
BrickGrass <49255229+BrickGrass@users.noreply.github.com>
2022-12-08 01:33:30 +0000

    Rewrote flask proxy to use simple database pooling

--- fps_proxy.py
+++ fps_proxy.py
@@ -3,7 +3,7 @@ import json
 
 from bs4 import BeautifulSoup
 from redis import Redis
-from flask import Flask, jsonify
+from flask import Flask, jsonify, g
 import psycopg2
 from typing import Optional
 
@@ -16,91 +16,104 @@ with open("config.json") as f:
     database_password = data["database_password"]
 
 sess = fps_get.Session()
-app = Flask(__name__)
 cache_time = timedelta(days=2)
 r = Redis()
 
-# TODO: Can't remember if using a single cursor for everything gets handled sensibly by flask
-# what with the multiple threads. I don't really have time to do it properly if so anyways. This might
-# be good if I really do need to: https://gist.github.com/vulcan25/55ce270d76bf78044d067c51e23ae5ad
-conn = psycopg2.connect(
-    host="localhost",
-    dbname=database_name,
-    user=database_username,
-    password=database_password)
-cur = conn.cursor()
+def create_app():
+    app = Flask(__name__)
+
+    # Database connection pool, maximum of 20 connections
+    # From: https://gist.github.com/vulcan25/55ce270d76bf78044d067c51e23ae5ad
+    app.config["postgreSQL_pool"] = psycopg2.pool.SimpleConnectionPool(1, 20,
+        host="localhost",
+        dbname=database_name,
+        user=database_username,
+        password=database_password)
+
+    def get_db():
+        if "db" not in g:
+            g.db = app.config["postgreSQL_pool"].getconn()
+        return g.db
+
+    def fetch_author(username: str) -> bool:
+        """Discover if an author has blanket permission from the local database and redis cache"""
+        username = username.lower()
+
+        value = r.get(username)
+        if value:
+            return False if value == b"n" else True
+
+        db = get_db()
+        cur = db.cursor()
+        cur.execute("SELECT username FROM users WHERE username = %s", (username,))
+        row = cur.fetchone()
+        cur.close()
+
+        if row:
+            r.setex(username, cache_time, "y")
+            return True
+
+        r.setex(username, cache_time, "n")
+        return False
 
 
-def fetch_author(username: str) -> bool:
-    """Discover if an author has blanket permission from the local database and redis cache"""
-    username = username.lower()
+    def fetch_author_from_web(username: str) -> Optional[dict]:
+        """Discover if an author has an fpslist.org page
 
-    value = r.get(username)
-    if value:
-        return False if value == b"n" else True
+        TODO: Might be a good idea to still cache this, just under a separate prefix from the other data.
+        data.<username> or something like that.
+        """
+        try:
+            data = sess.get_author(username)
+        except fps_get.NonceExpiredError:
+            sess.form.nonce = sess.get_nonce()
+            data = sess.get_author(username)
 
-    cur.execute("SELECT username FROM users WHERE username = %s", (username,))
-    row = cur.fetchone()
+        if data["recordsFiltered"] != 0:
+            for _, author_page, _, _, _, _ in data["data"]:
+                author_page = BeautifulSoup(author_page)
 
-    if row:
-        r.setex(username, cache_time, "y")
-        return True
+                if author_page.string.lower() == username.lower():
+                    author_data = author_page.a["href"]
+                    return {"author": author_data}
 
-    r.setex(username, cache_time, "n")
-    return False
+        return None
 
+    @app.teardown_appcontext
+    def close_conn(e):
+        print("Closing database connection")
+        db = g.pop("db", None)
+        if db is not None:
+            # Relinquish all connections to db pool
+            app.config["postpreSQL_pool"].putconn(db)
 
-def fetch_author_from_web(username: str) -> Optional[dict]:
-    """Discover if an author has an fpslist.org page
+    @app.route("/bp_api/author_exists/<username>")
+    def author_exists(username):
+        author = fetch_author(username)
+        return jsonify({"exists": author}), 200
 
-    TODO: Might be a good idea to still cache this, just under a separate prefix from the other data.
-    data.<username> or something like that.
-    """
-    try:
-        data = sess.get_author(username)
-    except fps_get.NonceExpiredError:
-        sess.form.nonce = sess.get_nonce()
-        data = sess.get_author(username)
+    @app.route("/bp_api/author_data/<username>")
+    def author_data(username):
+        author = fetch_author_from_web(username)
 
-    if data["recordsFiltered"] != 0:
-        for _, author_page, _, _, _, _ in data["data"]:
-            author_page = BeautifulSoup(author_page)
+        if not author:
+            return jsonify({"message": "not found"}), 201
 
-            if author_page.string.lower() == username.lower():
-                author_data = author_page.a["href"]
-                return {"author": author_data}
-
-    return None
-
-
-@app.route("/bp_api/author_exists/<username>")
-def author_exists(username):
-    author = fetch_author(username)
-    return jsonify({"exists": author}), 200
+        author["message"] = "found"
+        return jsonify(author), 200
 
 
-@app.route("/bp_api/author_data/<username>")
-def author_data(username):
-    author = fetch_author_from_web(username)
-
-    if not author:
-        return jsonify({"message": "not found"}), 201
-
-    author["message"] = "found"
-    return jsonify(author), 200
-
-
-@app.route("/bp_api/cache_health")
-def cache_health():
-    redis_info = r.info()
-    hits = redis_info.get("keyspace_hits")
-    misses = redis_info.get("keyspace_misses")
-    return jsonify({
-        "hits": hits,
-        "misses": misses,
-        "used_memory": redis_info.get("used_memory_human"),
-        "evicted_keys": redis_info.get("evicted_keys"),
-        "expired_keys": redis_info.get("expired_keys"),
-        "keys": redis_info.get("db0").get("keys"),
-        "hit_miss_ratio": f"{(hits / (hits + misses)) * 100:.2f}%"
-    }), 200
+    @app.route("/bp_api/cache_health")
+    def cache_health():
+        redis_info = r.info()
+        hits = redis_info.get("keyspace_hits")
+        misses = redis_info.get("keyspace_misses")
+        return jsonify({
+            "hits": hits,
+            "misses": misses,
+            "used_memory": redis_info.get("used_memory_human"),
+            "evicted_keys": redis_info.get("evicted_keys"),
+            "expired_keys": redis_info.get("expired_keys"),
+            "keys": redis_info.get("db0").get("keys"),
+            "hit_miss_ratio": f"{(hits / (hits + misses)) * 100:.2f}%"
+        }), 200
